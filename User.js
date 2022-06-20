946b445552afb4bde3d21cb285da2b51a62992cd
pryority <matthewapryor@gmail.com>
2022-06-19 22:56:32 -0400

    add Uesr schema/model

---
+++ User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought' }],
+        thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.replies.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
